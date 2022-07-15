86f75a77d587c2638ef5100d0038b43b6b02bb6e
Will Flores <willflores1221@gmail.com>
2022-07-15 09:38:47 -0400

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [name, setName] = useState("");
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	// On every render
+	useEffect(() => {
+		console.log("I re-rendered");
+	}, []);
+
+	// On first render/Mount only! - componentdidMount alternative
+	useEffect(() => {
+		console.log("The component mounted");
+	}, []);
+
+	// On first Render + whenever dependacy changes! - componentDidUpdate alternative
+	useEffect(() => {
+		console.log(`The name changed! ${name}`);
+
+		return () => {
+			//clean-up...
+			console.log("We unmounted!");
+		};
+	}, [name]);
+
+	// FOllows the same rules, except this handles the unmounting on a component! - componentWillUnmount Alternative
+	useEffect(() => {
+		console.log("Attach listener");
+		window.addEventListener("resize", updateWindowWidth);
+	}, []);
+
+	const updateWindowWidth = () => {
+		setWindowWidth(window.innerWidth);
+	};
+
+	return (
+		<div className="App">
+			<center>
+				<h1>The useEffect Hook</h1>
+				<h2>The window width is: {windowWidth}</h2>
+
+				<input
+					value={name}
+					onChange={(e) => setName(e.target.value)}
+					placeholder="Enter a name"
+				/>
+			</center>
+		</div>
+	);
 }
 
 export default App;
